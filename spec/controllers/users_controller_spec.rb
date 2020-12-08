02824039832cabd9c2783dbf68e99b0c40182c15
Rangga Permana <ranggapermana@catalyst.net.nz>
2020-12-08 15:02:29 +1300

    fix unit test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -415,15 +415,11 @@ describe UsersController, type: :controller do
       user = create(:user)
       @request.session[:user_id] = user.id
 
-      expect(user.user_settings[0][:value]).to eql("false")
+      expect(user.user_settings[0]).to eql(nil)
 
-      opts = {}
-      opts[0] = {}
-      opts[0][:id] = user.user_settings[0][:id]
-      opts[0][:value] = true
       params = {
         user: {
-          user_settings_attributes: opts,
+          "userdata-bbb_listen_only_mode": 1,
         }
       }
       post :update_settings, params: params.merge!(user_uid: user)
@@ -431,29 +427,46 @@ describe UsersController, type: :controller do
 
       expect(user.user_settings[0][:value]).to eql("true")
       expect(flash[:success]).to be_present
+
+      params = {
+        user: {
+          "userdata-bbb_listen_only_mode": 0,
+        }
+      }
+      post :update_settings, params: params.merge!(user_uid: user)
+      user.reload
+
+      expect(user.user_settings[0][:value]).to eql("false")
+      expect(flash[:success]).to be_present
     end
     it "should not update unrecognize user settings" do
       user = create(:user)
       @request.session[:user_id] = user.id
 
       settings_before = user.user_settings
-      opts = {}
-      opts[0] = {}
-      opts[0][:id] = "test"
-      opts[0][:value] = true
       params = {
         user: {
-          user_settings_attributes: opts,
+          "userdata-bbb_listen_only_mode_wrong": 1,
         }
       }
       post :update_settings, params: params.merge!(user_uid: user)
       user.reload
 
       expect(user.user_settings.length).to eql(settings_before.length)
-      expect(user.user_settings[0][:value]).to eql(settings_before[0][:value])
-      expect(user.user_settings[1][:value]).to eql(settings_before[1][:value])
-      expect(user.user_settings[2][:value]).to eql(settings_before[2][:value])
-      expect(user.user_settings[3][:value]).to eql(settings_before[3][:value])
+      expect(user.user_settings[0]).to eql(nil)
+    end
+    it "redirect to root if user isn't signed in" do
+      user = create(:user)
+      @request.session[:user_id] = user.id
+
+      settings_before = user.user_settings
+      params = {
+        user: {
+          "userdata-bbb_listen_only_mode": 1,
+        }
+      }
+      post :update_settings, params: params.merge!(user_uid: user)
+      expect(response).to redirect_to(root_path)
     end
   end
 
